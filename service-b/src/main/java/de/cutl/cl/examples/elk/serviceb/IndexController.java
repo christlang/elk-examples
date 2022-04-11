9dcbd5cd251ab4c4fabfd8cc1b0758fc364d2677
lange <lange@synyx.de>
2022-04-11 18:03:28 +0200

    log collection is working

---
+++ service-b/src/main/java/de/cutl/cl/examples/elk/serviceb/IndexController.java
@@ -0,0 +1,16 @@
+package de.cutl.cl.examples.elk.serviceb;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        log.info("called index");
+        return "index";
+    }
+}
