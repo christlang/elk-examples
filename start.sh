670288cb0ab63fc4f42d22f9fb431893379e2940
lange <lange@synyx.de>
2022-04-12 07:33:45 +0200

    update to 8.1.2 with more security

--- start.sh
+++ start.sh
@@ -4,4 +4,10 @@ sudo sysctl -w vm.max_map_count=262144
 
 docker-compose up -d
 
-docker-compose logs -f
+echo "do setup in localhost:5601"
+docker-compose exec elasticsearch bin/elasticsearch-create-enrollment-token --scope kibana
+docker-compose exec kibana bin/kibana-verification-code
+echo "create passwords"
+docker-compose exec elasticsearch bin/elasticsearch-setup-passwords interactive
+
+# docker-compose logs -f
