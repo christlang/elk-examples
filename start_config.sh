6baf59b1a17ba80001fd167391dfba661ad855ae
lange <lange@synyx.de>
2022-07-08 20:28:25 +0200

    testing metricbeat dashboard - not working at the moment

--- start_config.sh
+++ start_config.sh
@@ -19,3 +19,16 @@ docker run \
 
 
 # https://github.com/elastic/uptime-contrib
+
+
+# https://www.elastic.co/guide/en/beats/metricbeat/current/running-on-docker.html#_run_the_metricbeat_setup
+
+docker run \
+docker.elastic.co/beats/metricbeat:$ELASTICSEARCH_VERSION  \
+  setup                                                    \
+  -E setup.kibana.host=$KIBANA_URL                         \
+  -E setup.kibana.username=$KIBANA_USERNAME                \
+  -E setup.kibana.password=$KIBANA_PASSWORD                \
+  -E output.elasticsearch.hosts=[$ELASTICSEARCH_URL]       \
+  -E output.elasticsearch.username=$ELASTICSEARCH_USERNAME \
+  -E output.elasticsearch.password=$ELASTICSEARCH_PASSWORD
